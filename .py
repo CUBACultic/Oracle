d7fb5a9c34ce3c723bc7c7727df62df8cb0df203
CUBACultic <45536754+CUBACultic@users.noreply.github.com>
2018-12-02 23:05:20 +0000

    Create .py

---
+++ .py
@@ -0,0 +1,40 @@
+import random
+import time
+
+
+oraclelist = ["Yes","No",
+              "Maybe","Never",
+              "Try again","Oops",
+              "I am tired"]
+
+
+r= random.randint(0,6)
+i=0
+
+def oracle():
+    input("Enter your question...> ")
+    print(oraclelist[r])
+    time.sleep(1)
+
+def dieOracle():
+    yn =input("Try again?...> ")
+    if yn=="Yes" or yn=="Y":
+        oracle()
+    elif yn=="No"or yn=="N":
+        print("See you soon")
+        quit()
+        
+    else:
+        while True:
+            ny=input("Invalid input, Yes or No..> ")
+            if ny=="Yes" or "Y":
+                oracle()
+            elif ny=="No"or "N":
+                quit()
+        
+    
+print("Welcome...")
+time.sleep(1)
+oracle()
+dieOracle()
+time.sleep(1)
